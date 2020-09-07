82828508274a723090480ff5ae92ac2a99e43ea0
Daniel <dannygulland@gmail.com>
2020-09-07 11:53:28 -0700

    start lab 3

---
+++ Lab3/475_Lab_3/475_Lab_3/Stock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _475_Lab_3
+{
+    class Stock
+    {
+        private string Name { get; set;}
+        private int InitValue { get; set; }
+        private int MaxChange { get; set; }
+        private int NotificationThreshold { get; set; }
+
+        public Stock(String name, int initValue, int maxChange, int notificationThreshold)
+        {
+            this.Name = name;
+            this.InitValue = initValue;
+            this.MaxChange = maxChange;
+            this.NotificationThreshold = notificationThreshold;
+        }
+    }
+}
---
+++ Lab3/475_Lab_3/475_Lab_3/StockBroker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _475_Lab_3
+{
+    class StockBroker
+    {
+        private string Name { get; set; }
+        public StockBroker(string name)
+        {
+            this.Name = name;
+        }
+
+        public void AddStock(Stock stock)
+        {
+
+        }
+    }
+}
